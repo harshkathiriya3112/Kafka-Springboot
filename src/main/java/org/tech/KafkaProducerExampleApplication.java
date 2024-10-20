0cd365321b2293a36ef09ecf41e095a9ac1ae5d5
harshkathiriya3112 <harshdkathiriya@gmail.com>
2024-10-20 19:31:48 +0530

    publish API for sending data to topic & testing done

---
+++ src/main/java/org/tech/KafkaProducerExampleApplication.java
@@ -0,0 +1,11 @@
+package org.tech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaProducerExampleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaProducerExampleApplication.class,args);
+    }
+}
